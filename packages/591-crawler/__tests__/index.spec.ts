b89dc7fbf711e1ee02088a680626312a787fcff3
Yukai Huang <yukaihuangtw@gmail.com>
2019-01-27 12:12:04 +0800

    Organize api output

--- packages/591-crawler/__tests__/index.spec.ts
+++ packages/591-crawler/__tests__/index.spec.ts
@@ -1,7 +1,20 @@
+import { assert } from 'chai'
 import Crawler from '..'
 
 test('The crawler runs', async () => {
   const crawler = new Crawler()
   const data = await crawler.list()
-  expect(data).toBeTruthy()
+
+  assert.isOk(data)
+  assert.isArray(data.data)
+  assert.isNumber(data.pageNum)
+  assert.isNumber(data.total)
+})
+
+test('Test pagination', async function () {
+  const crawler = new Crawler()
+  const result1 = await crawler.list()
+  const result2 = await crawler.list({ page: 1 })
+
+  assert.notDeepEqual(result1.data[0], result2.data[1])
 })
--- packages/591-crawler/src/index.ts
+++ packages/591-crawler/src/index.ts
@@ -1,4 +1,36 @@
 import { default as axios, AxiosInstance } from 'axios'
+import { Data, TopData } from './types'
+import * as querystring from 'querystring'
+
+interface Option {
+  page?: number
+}
+
+class CrawlerOptions {
+  constructor (private options : Option) { }
+
+  toParams () {
+    return querystring.stringify({
+      firstRow: this.options.page
+    })
+  }
+}
+
+type ListResult = {
+  total: number
+  pageNum: number
+  data: Array<Data>
+}
+
+interface ListResponse {
+  status: number,
+  data: {
+    topData: Array<TopData>,
+    data: Array<Data>
+  },
+  records: string,
+  is_recom: number
+}
 
 export default class Crawler {
   private client: AxiosInstance;
@@ -10,8 +42,17 @@ export default class Crawler {
     })
   }
 
-  async list () {
-    const { data } = await this.client.get('/home/search/rsList')
-    return data
+  async list (opt: Option = {}): Promise<ListResult> {
+    const options = new CrawlerOptions(opt)
+    const { data } = await this.client.get(`/home/search/rsList?${options.toParams()}`)
+    let listData: ListResponse = data
+
+    const total = parseInt(listData.records)
+
+    return {
+      total,
+      pageNum: total / 30,
+      data: listData.data.data
+    }
   }
 }
